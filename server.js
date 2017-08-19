6416f7c81043d36be523224e7702848f8e8545b2
priyalanka74 <31090084+priyalanka74@users.noreply.github.com>
2017-08-19 15:04:37 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var content ={
+var Article1 ={
     title :'Article1 , Supriya',
     heading: 'Article one',
     date : 'Aug 19th',
