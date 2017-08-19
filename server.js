38c501f6ace6869237235f82685ca5c51e71b382
priyalanka74 <31090084+priyalanka74@users.noreply.github.com>
2017-08-18 21:37:32 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/ui/style.css', function (req, res) {
+//app.get('/ui/style.css', function (req, res) {
+app.get('/article-one', function (req, res){
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
