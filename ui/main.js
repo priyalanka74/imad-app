d0293318455e9d02b8170af412df7c17ec06e957
priyalanka74 <31090084+priyalanka74@users.noreply.github.com>
2017-08-21 16:25:51 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,11 @@ console.log('Loaded!');
 var element=document.getElementById("main-text");
 element.innerHTML = "New Test Value";
 var img = document.getElementById("madi");
-img.onclick = function(){
-    img.style.marginleft='200px';
+var marginleft = 0;
+function moveRight(){
+    marginleft = marginleft+10;
+    img.style.marginleft = marginleft+'px';
+}
+img.onclick= function(){
+var interval = serinterval(moveRight,50);
 };
