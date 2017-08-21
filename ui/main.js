d4b60e8829e5912001400e3b1563322caba4d8b0
priyalanka74 <31090084+priyalanka74@users.noreply.github.com>
2017-08-21 16:12:55 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,5 +3,5 @@ var element=document.getElementById("main-text");
 element.innerHTML = "New Test Value";
 var img = document.getElementById("madi");
 img.onclick = function(){
-    img.style.marginleft="200px";
+    img.style.marginleft='200px';
 };
